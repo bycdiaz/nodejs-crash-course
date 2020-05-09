a5399300f63b811ce58587a88fb7a8cb1585b65a
Carlos Diaz <cdiaz86@gmail.com>
2020-05-08 22:13:16 -0400

    Passes JSON through users url.

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const server = http.createServer((req, res) => {
       res.writeHead('200', { 'Content-Type': 'text/html' });
       res.end(content);
     })
-  } else if (req.url === '/about') {
-    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-      if (err) throw err;
-      res.writeHead('200', { 'Content-Type': 'text/html' });
-      res.end(content);
-    })
-  } else {
-    res.writeHead('200', { 'Content-Type': 'text/html' });
-    res.end("Page doesn't exist!");
+  }
+
+  if (req.url === '/api/users') {
+    const users = [
+      { name: 'Bob Smith', age: 40 },
+      { name: 'John Doe', age: 30 },
+    ];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users))
   }
 });
 
